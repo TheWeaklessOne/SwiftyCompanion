c46a121c5cabbc7612cb2051555b32f1f3949c19
Daniil Teterin <DVlTeterin@sberbank.ru>
2021-06-29 11:43:49 +0300

    Initial commit

---
+++ Swifty Companion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swifty Companion
+//
+//  Created by Даниил Тетерин on 29.06.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
